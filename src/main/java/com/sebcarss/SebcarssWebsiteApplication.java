2a3324e18c95348e0927f868daa78744b0686657
Robert Carss <sebcarss@mekong.home>
2017-08-12 17:52:01 +0100

    Initial setup of Spring Boot app from ItelliJ

---
+++ src/main/java/com/sebcarss/SebcarssWebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.sebcarss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SebcarssWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SebcarssWebsiteApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sebcarss/SebcarssWebsiteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sebcarss;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SebcarssWebsiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
