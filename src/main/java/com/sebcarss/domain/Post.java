9ac52fd3108ef0d207b6d05ddc8003194a05a952
Seb Carss <seb.carss@gmail.com>
2018-01-03 20:46:03 +0000

    Clean up ramen post page of lorum ipsum and add date fields to Post

--- src/main/java/com/sebcarss/domain/Post.java
+++ src/main/java/com/sebcarss/domain/Post.java
@@ -9,6 +9,9 @@ public class Post {
     @Id
     private long id;
     private String title;
+    private short year;
+    private byte month;
+    private byte day;
 
     @Override
     public String toString() {
@@ -29,5 +32,29 @@ public class Post {
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public short getYear() {
+        return year;
+    }
+
+    public void setYear(short year) {
+        this.year = year;
+    }
+
+    public byte getMonth() {
+        return month;
+    }
+
+    public void setMonth(byte month) {
+        this.month = month;
+    }
+
+    public byte getDay() {
+        return day;
+    }
+
+    public void setDay(byte day) {
+        this.day = day;
     }
 }
