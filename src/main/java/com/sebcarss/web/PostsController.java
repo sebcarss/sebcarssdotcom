56580c75de3653c1455473da919464d3f918f0d4
Seb Carss <seb.carss@gmail.com>
2018-03-11 11:49:30 +0000

    Replace strings with constants

--- src/main/java/com/sebcarss/web/PostsController.java
+++ src/main/java/com/sebcarss/web/PostsController.java
@@ -7,9 +7,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class PostsController {
+
+    public static final String POST = "post";
+    public static final String REDIRECT_HOME = "redirect:/";
 
     @Autowired
     private PostRepository postRepository;
@@ -17,7 +21,12 @@ public class PostsController {
     @RequestMapping("/{permalink}")
     public String getPostByPermalink(@PathVariable("permalink") String permalink, Model model) {
         Post post = (Post) postRepository.findByPermalink(permalink);
-        model.addAttribute("post", post);
-        return "post";
+
+        if (post != null) {
+            model.addAttribute("post", post);
+            return POST;
+        } else {
+            return REDIRECT_HOME;
+        }
     }
 }
